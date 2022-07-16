f82ba89625775000a45b337e34e3ac30a5be62c7
Bagus Kurnia <baguskurnia1010@gmail.com>
2022-07-16 20:48:20 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Bagus Kurnia on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
